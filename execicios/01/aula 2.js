c90dbe874b383e1fa1936692c75a2a42340b747f
unknown <andregroliveira@gmail.com>
2021-06-10 15:39:37 -0300

    primeiro commit andre

---
+++ execicios/01/aula 2.js
@@ -0,0 +1,19 @@
+// var a;
+// var b = 2;
+
+// a = 7;
+
+// b = a;
+
+// console.log (a);
+
+// Initialize these three variable
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
